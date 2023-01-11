356a9cdb2d80d05b2aa2b8f68394d3737e53d41f
Dani <dan.avila7@gmail.com>
2023-01-11 10:55:49 -0500

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,8 +38,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`AI inside your IDE`}
+      description="Improve your code with Code GPT <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
