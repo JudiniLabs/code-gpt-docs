33b263a9e8c0f9eee5f2256c2de01d41d3dde12c
Dani <dan.avila7@gmail.com>
2023-01-11 10:44:34 -0500

    hide some options

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Code GPT Docs',
-  tagline: 'Artificial intelligence inside your IDE',
+  title: 'Code GPT',
+  tagline: 'Artificial intelligence inside your IDE with this open source extension',
   url: 'https://code-gpt-docs.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -36,15 +36,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+            //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -74,12 +74,16 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          //{to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/davila7/code-gpt-docs',
-            label: 'GitHub',
-            position: 'right',
+            to: 'https://marketplace.visualstudio.com/items?itemName=DanielSanMedium.dscodegpt', 
+            label: 'Extension', 
+            position: 'left'
           },
+          // {
+          //   href: 'https://github.com/davila7/code-gpt-docs',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
           // {
           //   type: 'localeDropdown',
           // },
