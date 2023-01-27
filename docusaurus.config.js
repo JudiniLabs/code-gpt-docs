520912e91ba74330e359a1275a8c65665c8c13b7
Dani <dan.avila7@gmail.com>
2023-01-27 18:06:05 -0500

    Subscribe for updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,10 +129,10 @@ const config = {
           {
             title: 'More',
             items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
+              {
+                label: 'Subscribe for updates',
+                to: 'https://817715f5.sibforms.com/serve/MUIEAMf2kzgJKDLrfbc46jFqp1U2BCxtNd2G2YfHO_4Wdqvdj0FqFJzy7a-_iVFmCKBzuSIjfoqot4O8DJXEj288d_YmAXXYKQCe34E0CseCIq7Ozvg90PktET7eeGdlTtrSc5f4S4pMYkyrJdQRSjLyur5_l1r3mUyCbDahOq4M8Jc5Hy-c9ZX_wkX-qt5lH0ORt6ePXfyRrGZY',
+              },
               {
                 label: 'Code GPT Marketplace',
                 href: 'https://marketplace.visualstudio.com/items?itemName=DanielSanMedium.dscodegpt',
