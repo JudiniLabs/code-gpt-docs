eff5e4560eab7499ddb1a85fc61e4f0ab05d98d3
PilarHidalgo <37567214+PilarHidalgo@users.noreply.github.com>
2024-11-06 14:08:29 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const examplesPath = path.resolve(__dirname, "..", "examples", "src");
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-@type {import('@docusaurus/types').Config} 
+//@type {import('@docusaurus/types').Config} 
 const config = {
   title: 'CodeGPT',
   tagline: 'Easily Connect to Top AI Providers Using Their Official APIs in VSCode',
@@ -27,7 +27,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['pt'],//'en', 'es', 'pt', 'fr', 'ko', 'zh-Hans'],
+    locales: ['pt']//'en', 'es', 'pt', 'fr', 'ko', 'zh-Hans'],
   },
   customFields:{
     mendableAnonKey: process.env.MENDABLE_ANON_KEY,
